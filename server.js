83f9096dec1911e0ed18f734f2560c08160a76f2
Cédric Bazureau <cbazureau@gmail.com>
2016-05-11 21:17:53 +0200

    Appairage TV/Mobile

---
+++ server.js
@@ -0,0 +1,70 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8082
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+var server_baseurl = (process.env.OPENSHIFT_NODEJS_IP) ? "http://doublenav-lebeaujeu.rhcloud.com": "http://192.168.1.44:8082";
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var connections = [];
+
+function randomString(length) {
+    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/mobile', function(req, res){
+  res.sendFile(__dirname + '/mobile.html');
+});
+
+io.on('connection', function(socket){
+  // When TV ask for à new QR Code
+  socket.on('tv-get-qr', function(msg){
+    var r = randomString(20);
+    var connection = {
+        id: r,
+        socket_tv: socket.id,
+        socket_mobile: null,
+        url: (server_baseurl+"/mobile?qr=" + r + "&test=32")
+    };
+    console.log(connection);
+    connections.push(connection);
+    if(connections.length>10) connections.splice(0, (10-connections.length));
+    socket.emit("tv-receive-qr",connection.url);
+  });
+  
+  
+  socket.on('mobile-infos', function(infos){
+      for(var i=0;i<connections.length;i++) {
+         if(socket.id==connections[i].socket_mobile) { 
+            io.sockets.connected[connections[i].socket_tv].emit("tv-infos",infos); 
+         }
+      }
+  });
+  
+  socket.on('mobile-attach', function(qr){
+      console.log("QR :"+qr);
+      
+      for(var i=0;i<connections.length;i++) {
+          console.log(connections[i].id);
+          if(connections[i].id==qr) {
+              connections[i].socket_mobile = socket.id;
+              console.log("socket_tv :"+connections[i].socket_tv);
+              console.log("socket_mobile :"+connections[i].socket_mobile);
+              
+              // Send to tv and mobile
+              socket.emit("mobile-attach-success");
+              io.sockets.connected[connections[i].socket_tv].emit("tv-attach-success");
+          }
+      }
+      
+  });
+});
+
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
